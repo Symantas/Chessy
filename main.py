5a551737ea32c1e53ff03f513354d91e26fce941
Symantas <symantasstatkevicius@gmail.com>
2026-08-07 18:13:19 +0300

    Initial bot setup with one extra command

---
+++ main.py
@@ -0,0 +1,33 @@
+import discord
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv(DISCORD_TOKEN)
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+     print(f'I have logged in as {client.user}')
+
+@client.event 
+async def on_message(message):
+     if message.author == client.user:
+         return
+    
+     if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+        
+        
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$exit'):
+       await message.channel.send('Exiting...')
+       await client.close()
+        
+client.run(TOKEN)
