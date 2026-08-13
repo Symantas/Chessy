738d87ae3314296ad03e623139db7a4b6acb715b
Symantas <symantasstatkevicius@gmail.com>
2026-08-13 19:56:20 +0300

    Implemented live Rapid pull from chess.com, added embed

--- main.py
+++ main.py
@@ -1,33 +1,50 @@
 import discord
 import os
+import aiohttp 
+import asyncio
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
-TOKEN = os.getenv(DISCORD_TOKEN)
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='$', intents=intents)
 
-@client.event
-async def on_ready():
-     print(f'I have logged in as {client.user}')
 
-@client.event 
-async def on_message(message):
-     if message.author == client.user:
-         return
+@bot.command(name='hello')
+async def greeting(ctx):
+    await ctx.send('Hello!')
     
-     if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-        
-        
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$exit'):
-       await message.channel.send('Exiting...')
-       await client.close()
-        
-client.run(TOKEN)
+
+@bot.command(name='exit')
+async def exiting(ctx):
+    await ctx.send('exiting...')
+    await bot.close()
+    
+
+@bot.command(name='stats')
+async def show_player_stats(ctx, arg):
+    stats_url = f"https://api.chess.com/pub/player/{arg}/stats"
+    
+    async with aiohttp.ClientSession() as session:
+        async with session.get(stats_url) as response:
+            data = await response.json()
+            
+            embed = discord.Embed( 
+                title=f"Chess.com Stats for {arg}",
+                description="Stats are pulled from live Rapid rating",
+                color=discord.Color.green()                    
+            )
+            
+            embed.add_field(
+                name="Rapid ⏱️",
+                value=str(data['chess_rapid']['last']['rating']),
+                inline=True
+            )
+            
+            await ctx.send(embed=embed)
+            
+            
+bot.run(TOKEN)
